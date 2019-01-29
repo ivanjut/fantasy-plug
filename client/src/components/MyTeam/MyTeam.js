4198cfa0ad963042f5ceabaf00abe79dada1d091
Ivan Jutamulia <ivanjutamulia@Ivans-MBP.attlocal.net>
2019-01-29 10:39:42 -0800

    started working on my team

--- client/src/components/MyTeam/MyTeam.js
+++ client/src/components/MyTeam/MyTeam.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import './MyTeam.css';
 import { Button, Modal, ModalBody, ModalHeader, ModalFooter } from 'mdbreact';
+import Chip from '@material-ui/core/Chip';
 
 class MyTeam extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchField: "",
+        };
+    };
 
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+    };
+
+    handleDeletePlayer = (player) => {
+
+    };
+
+    render() {
         return (
             <div className='modal-container'>
                  <Modal isOpen={this.props.isOpen} toggle={this.props.toggle} centered>
@@ -12,11 +27,30 @@ class MyTeam extends Component {
                         <ModalBody>
                             <p>Organize your team here!</p>
                             <hr/>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            <input className="form-control mr-sm-2" type="search" placeholder="search players" aria-label="Search"
+                                   onChange={this.onSearchChange}/>
+                            <div className='my-team-container'>
+                                <div className='position-container'>
+                                    <h5><strong>QB</strong></h5>
+                                    <div className='team-qbs player-chips'>
+                                        <Chip   label="Deletable Primary Chip"
+                                                onDelete={this.handleDeletePlayer}
+                                                className='deletable-chip'
+                                                color="primary"/>
+                                        <Chip   label="Another Test"
+                                                onDelete={this.handleDeletePlayer}
+                                                className='deletable-chip'
+                                                color="primary"/>
+                                        <Chip   label="Saquon Barkley"
+                                                onDelete={this.handleDeletePlayer}
+                                                className='deletable-chip'
+                                                color="primary"/>
+                                    </div>
+                                </div>
+                            </div>
                         </ModalBody>
                         <ModalFooter>
                             <Button color="secondary" onClick={this.props.toggle}>Close</Button>
-                            <Button color="primary" onClick={this.props.toggle}>Save</Button>
                         </ModalFooter>
                  </Modal>
             </div>
